7d1afbc872bd36b8207721447b1e6ede199f833f
Vipin Kumar <vipink1203@gmail.com>
2020-05-12 23:09:11 +0530

    Updates

--- README.md
+++
@@ -1,9 +0,0 @@
-# RoboFriends - React App
-
-## To Run this app:
-
-```
-$ git clone https://github.com/vipink1203/robofriends-react.git
-$ cd robofriends-react
-$ npm start
-```
---
+++ precache-manifest.cd4f8ede04ca40e9acbfc0a580808f14.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e23b53d3a06ec657c769062a0fb1bdcd",
+    "url": "/robofriends-react/index.html"
+  },
+  {
+    "revision": "9230a8b77f7062058b09",
+    "url": "/robofriends-react/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a5e49a4df5716b7b6409",
+    "url": "/robofriends-react/static/css/main.ba2677ab.chunk.css"
+  },
+  {
+    "revision": "9230a8b77f7062058b09",
+    "url": "/robofriends-react/static/js/2.2b82f756.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends-react/static/js/2.2b82f756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5e49a4df5716b7b6409",
+    "url": "/robofriends-react/static/js/main.bb9c6b5b.chunk.js"
+  },
+  {
+    "revision": "1815f2d1560e68ec42a7",
+    "url": "/robofriends-react/static/js/runtime-main.276ee05e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-react/static/media/SEGA.12a3db8b.woff"
+  }
+]);
